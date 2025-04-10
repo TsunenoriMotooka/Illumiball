820646ea985396b82dc1c5f12de23e82e7fe592e
TsunenoriMotooka <tsune.motooka@gmail.com>
2025-04-10 11:39:33 +0900

    仮実装

---
+++ Assets/Scripts/GravityController.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityController : MonoBehaviour
+{
+    const float Gravity = 9.81f;
+    
+    [SerializeField]
+    float gravityScale = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 vector = new Vector3();
+
+        if (Application.isMobilePlatform)
+        {
+            vector.x = Input.acceleration.x;
+            vector.z = Input.acceleration.y;
+            vector.y = Input.acceleration.z;
+        }
+        else
+        {
+            vector.x = Input.GetAxis("Horizontal");
+            vector.z = Input.GetAxis("Vertical");
+
+            if (Input.GetKey("z"))
+            {
+                vector.y = 1.0f;
+            }
+            else
+            {
+                vector.y = -1.0f;
+            }
+        }
+
+        Physics.gravity = Gravity * vector.normalized * gravityScale;
+    }
+}
