486680dee8c2ac70a3353ebe83c48671a31c318d
TsunenoriMotooka <tsune.motooka@gmail.com>
2025-04-10 13:15:01 +0900

    ホールの実装

---
+++ Assets/Scripts/Hole.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hole : MonoBehaviour
+{
+    public string targetTag;
+
+    void OnTriggerStay(Collider other)
+    {
+        Rigidbody r = other.gameObject.GetComponent<Rigidbody>();
+
+        //ホールからボールへのベクトル
+        Vector3 direction = other.gameObject.transform.position - transform.position; 
+        //長さを１にする
+        direction.Normalize();
+        
+        if (other.CompareTag(targetTag))
+        {
+            r.velocity *= 0.9f;
+            r.AddForce(direction * -20.0f, ForceMode.Acceleration);        
+        }
+        else
+        {
+            r.AddForce(direction * 80f, ForceMode.Acceleration);
+        }    
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
